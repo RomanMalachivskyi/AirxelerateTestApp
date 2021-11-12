6f445153ec21816ebb070b722fa065cba72a8328
Roman Malachivskyi <romanmal@ext.inditex.com>
2021-11-12 14:53:42 +0200

    first commit

---
+++ src/main/java/com/airxelerate/AirxelerateTestApp/AirxelerateTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.airxelerate.AirxelerateTestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirxelerateTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirxelerateTestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airxelerate/AirxelerateTestApp/AirxelerateTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airxelerate.AirxelerateTestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirxelerateTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
